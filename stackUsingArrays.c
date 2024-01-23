172af401bc0bd37986cc40e0e8b37782211a5c57
Devansh334 <devanshgoyal334@gmail.com>
2024-01-24 00:20:05 +0530

    Stack Implementation Using Arrays0

---
+++ stackUsingArrays.c
@@ -0,0 +1,89 @@
+// Stack Using Arrays
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define CAPACITY 5
+
+int stack[CAPACITY];
+int top = -1;
+
+bool isEmpty()
+{
+    if (top == -1)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool isFull()
+{
+    if (top == CAPACITY - 1)
+    {
+        return true;
+    }
+    return false;
+}
+
+void push(int data)
+{
+    if (isFull())
+    {
+        printf("Stack Overflow");
+        return;
+    }
+
+    stack[++top] = data;
+}
+
+int pop()
+{
+    if (isEmpty())
+    {
+        printf("Stack Underflow");
+        return -1;
+    }
+    return stack[top--];
+}
+
+int peek()
+{
+    if (isEmpty())
+    {
+        printf("Empty Stack");
+        return -1;
+    }
+    return stack[top];
+}
+
+void printStack()
+{
+    if (isEmpty())
+    {
+        printf("Empty Stack");
+        return;
+    }
+
+    for (int i = top; i >= 0; i--)
+    {
+        printf("%d\n", stack[i]);
+    }
+}
+
+void main()
+{
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    push(50);
+    pop();
+    pop();
+    push(35);
+    push(45);
+    printStack();
+    printf("\n");
+    printf("Peek Element : %d", peek());
+}
